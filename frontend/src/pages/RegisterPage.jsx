0c865366295c0911d0f5d0a4d5e2166bba065080
AnteProlic <ante.prolic2002@gmail.com>
2023-11-07 18:00:27 +0100

    Dodan kostur i općenita struktura stranice za registraciju

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Template from "../components/Template";
+import Input from "../components/Input";
 import "../index.css"
 
 class RegisterPage extends React.Component {
@@ -8,12 +9,41 @@ class RegisterPage extends React.Component {
             {/* <Header profil={<Profil />}/> */}
             <Template>
                 <div>
-                    Registracija
-                    <div>
-                        <div>Osnovni podaci</div>
-                        <div>Kontakt podaci</div>
-                        <div>Podaci za prijavu</div>
-                        <div>Poslodavac</div>
+                    <div className="naslov">Registracija</div>
+                    <div id="register_main_container">
+                        <div>
+                            Osnovni podaci
+                            <div className="register_category_container">
+                                <Input tag="Ime" name="ime" placeholder="Ime"/>
+                                <Input tag="Prezime" name="prezime" placeholder="Prezime"/>
+                                <Input tag="OIB" name="oib" placeholder="OIB"/>
+                                <Input tag="Datum rođenja" name="datum_rod" placeholder="Odaberi datum"/>
+                                <Input tag="Mjesto prebivališta" name="mjesto_preb" placeholder="Odaberi mjesto"/>
+                                <Input tag="Poštanski broj prebivališta" name="post_broj" placeholder="Poštanski broj"/>
+                            </div>
+                        </div>
+                        <div>
+                            Kontakt podaci
+                            <div className="register_category_container">
+                                <Input tag="Broj telefona" name="broj_tel" placeholder="+385"/>
+                                <Input tag="Adresa elektroničke pošte" name="email" placeholder="Adresa elektroničke pošte"/>
+                        </div>
+                        </div>
+                        <div>
+                            Podaci za prijavu
+                            <div className="register_category_container">
+                                <Input tag="Lozinka" name="lozinka1" placeholder="Lozinka"/>
+                                <Input tag="Ponovi lozinku" name="lozinka2" placeholder="Ponovljena lozinka"/>
+                                <Input tag="Korisničko ime" name="username" placeholder="Korisničko ime"/>
+                            </div>
+                        </div>
+                        <div>
+                            Poslodavac
+                            <div className="register_category_container">
+                                <Input tag="E-mail poslodavca" name="poslodavac_mail" placeholder="E-mail poslodavca"></Input>
+                            </div>
+                        </div>
+                        <button id="register_button">REGISTRIRAJ SE</button>
                     </div>
                 </div>
             </Template>
