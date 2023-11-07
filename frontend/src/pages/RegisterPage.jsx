34d8c660f28a5fe82f5f397e5b59cadaef324a32
AnteProlic <ante.prolic2002@gmail.com>
2023-11-07 16:50:21 +0100

    changes

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Template from "../components/Template";
+import "../index.css"
+
+class RegisterPage extends React.Component {
+    render() {
+        return <div>
+            {/* <Header profil={<Profil />}/> */}
+            <Template>
+                <div>
+                    Registracija
+                    <div>
+                        <div>Osnovni podaci</div>
+                        <div>Kontakt podaci</div>
+                        <div>Podaci za prijavu</div>
+                        <div>Poslodavac</div>
+                    </div>
+                </div>
+            </Template>
+        </div>
+    }
+}
+
+export default RegisterPage
