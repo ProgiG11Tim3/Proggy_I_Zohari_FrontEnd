7c04c7b19c65c46b2bbd5bc65d07fefa567076b5
AnteProlic <ante.prolic2002@gmail.com>
2023-11-15 23:16:01 +0100

    Dodan submit check i submit funkcija na stranici za registraciju, popravljen dio CSS-a

--- frontend/src/components/components/Input.jsx
+++ frontend/src/components/components/Input.jsx
@@ -3,10 +3,17 @@ import React from "react";
 import "../../index.css"
 
 class Input extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return <div className="input_container">
             <div className="input_tag">{this.props.tag}</div>
-            <input className="input_box" type={this.props.type} name={this.props.name} placeholder={this.props.placeholder}></input>
+            <input className="input_box" 
+            type={this.props.type} 
+            name={this.props.name} 
+            placeholder={this.props.placeholder} onChange={this.props.handleChange}/>
         </div>
     }
 }
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -5,42 +5,100 @@ import InputsParagraf from "../components/InputsParagraf";
 import "../index.css"
 
 class RegisterPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nameParent: null,
+            lastNameParent: null,
+            oib: null,
+            dateOfBirthParent: null,
+            placeOfResidence: null,
+            postalCode: null,
+            phoneNumberParent: null,
+            emailParent: null,
+            userNameParent: null,
+            passwordParent: null,
+            passwordParent2: null,
+            employerEmail: null
+        }
+        this.submit = this.submit.bind(this)
+        this.checkInputs = this.checkInputs.bind(this)
+    }
+
+    submit() {
+        if (this.state.passwordParent != this.state.passwordParent2) {
+            alert("Lozinke se ne podudaraju, pokušajte ponovo.")
+        } else if (!this.checkInputs()) {
+            alert("Molimo da popunite formu u potpunosti.")
+        } else {
+            console.log(this.state)
+        }
+    }
+
+    checkInputs() {
+        for (const prop in this.state) {
+            if (this.state[prop] === null) {
+                return false
+            }
+        }
+        return true
+    }
+
     render() {
         return <div>
-            {/* <Header profil={<Profil />}/> */}
             <Template>
                 <div className="main">
                     <div className="main_naslov">Registracija</div>
                     <div id="register_main_container">
                         <InputsParagraf paragrafTitle="Osnovni podaci">
-                            <Input tag="Ime" name="ime" placeholder="Ime" type="text"/>
-                            <Input tag="Prezime" name="prezime" placeholder="Prezime" type="text"/>
-                            <Input tag="OIB" name="oib" placeholder="OIB" type="number"/>
-                            <Input tag="Datum rođenja" name="datum_rod" placeholder="Odaberi datum" type="date"/>
-                            <Input tag="Mjesto prebivališta" name="mjesto_preb" placeholder="Mjesto prebivališta" type="text"/>
-                            <Input tag="Poštanski broj prebivališta" name="post_broj" placeholder="Poštanski broj" type="text"/>
+                            <Input tag="Ime" name="ime" placeholder="Ime" type="text" 
+                            handleChange={e => {this.setState({nameParent: e.target.value})}} />
+
+                            <Input tag="Prezime" name="prezime" placeholder="Prezime" type="text"
+                            handleChange={e => {this.setState({lastNameParent: e.target.value})}}/>
+
+                            <Input tag="OIB" name="oib" placeholder="OIB" type="number"
+                            handleChange={e => {this.setState({oib: e.target.value})}}/>
+
+                            <Input tag="Datum rođenja" name="datum_rod" placeholder="Odaberi datum" type="date"
+                            handleChange={e => {this.setState({dateOfBirthParent: e.target.value})}}/>
+
+                            <Input tag="Mjesto prebivališta" name="mjesto_preb" placeholder="Mjesto prebivališta" type="text"
+                            handleChange={e => {this.setState({placeOfResidence: e.target.value})}}/>
+
+                            <Input tag="Poštanski broj prebivališta" name="post_broj" placeholder="Poštanski broj" type="text"
+                            handleChange={e => {this.setState({postalCode: e.target.value})}}/>
                         </InputsParagraf>
                         
                         <div className="line"></div>
                         <InputsParagraf paragrafTitle="Kontakt podaci">
-                            <Input tag="Broj telefona" name="broj_tel" placeholder="+385" type="number"/>
-                            <Input tag="Adresa elektroničke pošte" name="email" placeholder="Adresa elektroničke pošte" type="email"/>
+                            <Input tag="Broj telefona" name="broj_tel" placeholder="+385" type="number"
+                            handleChange={e => {this.setState({phoneNumberParent: e.target.value})}}/>
+                            
+                            <Input tag="Adresa elektroničke pošte" name="email" placeholder="Adresa elektroničke pošte" type="email"
+                            handleChange={e => {this.setState({emailParent: e.target.value})}}/>
                         </InputsParagraf>
 
                         <div className="line"></div>
 
                         <InputsParagraf paragrafTitle="Podaci za prijavu">
-                            <Input tag="Korisničko ime" name="username" placeholder="Korisničko ime" type="text"/>
-                            <Input tag="Lozinka" name="lozinka1" placeholder="Lozinka" type="password"/>
-                            <Input tag="Ponovi lozinku" name="lozinka2" placeholder="Ponovljena lozinka" type="password"/>
+                            <Input tag="Korisničko ime" name="username" placeholder="Korisničko ime" type="text"
+                            handleChange={e => {this.setState({userNameParent: e.target.value})}}/>
+
+                            <Input tag="Lozinka" name="lozinka1" placeholder="Lozinka" type="password"
+                            handleChange={e => {this.setState({passwordParent: e.target.value})}}/>
+
+                            <Input tag="Ponovi lozinku" name="lozinka2" placeholder="Ponovljena lozinka" type="password"
+                            handleChange={e => {this.setState({passwordParent2: e.target.value})}}/>
                         </InputsParagraf>
                         
                         <div className="line"></div>
 
                         <InputsParagraf paragrafTitle="Poslodavac">
-                            <Input tag="E-mail poslodavca" name="poslodavac_mail" placeholder="E-mail poslodavca" type="email"></Input>
+                            <Input tag="E-mail poslodavca" name="poslodavac_mail" placeholder="E-mail poslodavca" type="email" 
+                            handleChange={e => {this.setState({employerEmail: e.target.value})}}/>
                         </InputsParagraf>
-                        <button id="register_button">Registriraj se</button>
+                        <button id="register_button" onClick={this.submit}>Registriraj se</button>
                     </div>
                 </div>
             </Template>
