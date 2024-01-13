76b62c433006af829403924913f901fd295768bd
Kristina Čavlović <133607855+kristina-cavlovic@users.noreply.github.com>
2024-01-14 00:34:16 +0100

    LOM: NewPatient, PatientList, SickNotes
    bez navbar, header

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,9 @@ import './index.css';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ProfileDoctorPatientList from './pages/ProfileDoctorPatientList';
+import ProfileDoctorNewPatient from "./pages/ProfileDoctorNewPatient";
+import ProfileDoctorSickNotes from "./pages/ProfileDoctorSickNotes"
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -12,6 +15,9 @@ const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path={"/sicknotes"} element={<ProfileDoctorSickNotes />} />
+                <Route path={"/newpatient"} element={<ProfileDoctorNewPatient />} />
+                <Route path={"/patientlist"} element={<ProfileDoctorPatientList />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/" element={<MainPage />} />
--- frontend/src/pages/ProfileDoctorSickNotes.jsx
+++ frontend/src/pages/ProfileDoctorSickNotes.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Template from "../components/Template";
 import "../index.css";
 
-class ProfileDoctorPatientList extends React.Component {
+class ProfileDoctorSickNotes extends React.Component {
     render(){
         return <Template>
-            <div id={"patient_list_naslov"} className={"lom_naslovi"}> Popis pacijenata </div>
+            <div id={"patient_list_naslov"} className={"lom_naslovi"}> Preporuke za bolovanje </div>
             <div id={"patient_list_bigboy"}>
                 <div className={"patient_list_instance"}>
                     <div className={"patient_list_infobox"}>
@@ -13,7 +13,7 @@ class ProfileDoctorPatientList extends React.Component {
                         <div className={"patient_list_oib"}>12345678910</div>
                     </div>
                     <div className={"patient_list_buttonbox"}>
-                        <button className={"patient_list_button"}>Profil</button>
+                        <button className={"patient_list_button"}>Otvori</button>
                     </div>
                 </div>
                 <div className={"patient_list_instance"}>
@@ -22,7 +22,7 @@ class ProfileDoctorPatientList extends React.Component {
                         <div className={"patient_list_oib"}>12345678910</div>
                     </div>
                     <div className={"patient_list_buttonbox"}>
-                        <button className={"patient_list_button"}>Profil</button>
+                        <button className={"patient_list_button"}>Otvori</button>
                     </div>
                 </div>
 
@@ -32,4 +32,4 @@ class ProfileDoctorPatientList extends React.Component {
     }
 }
 
-export default ProfileDoctorPatientList
+export default ProfileDoctorSickNotes
