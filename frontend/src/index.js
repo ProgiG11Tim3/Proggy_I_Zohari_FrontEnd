64c08dfc464bac0c136f1b18abc959856403a774
Kristina Čavlović <133607855+kristina-cavlovic@users.noreply.github.com>
2024-01-13 16:46:05 +0100

    popravljeno take 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/" element={<MainPage />} />
             </Routes>
