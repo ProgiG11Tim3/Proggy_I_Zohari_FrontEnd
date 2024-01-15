3b203cb96b72ee10d504ac3b35a275f4022dc333
Kristina Čavlović <133607855+kristina-cavlovic@users.noreply.github.com>
2024-01-15 14:07:53 +0100

    Pedijatar: 8 pageova (kao LOM)
    pages, CSS, rute

---
+++ frontend/src/pages/ProfilePediatricianMedicalReport.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Template from "../components/Template";
+import "../index.css";
+import Input from "../components/components/Input";
+class ProfilePediatricianMedicalReport extends React.Component {
+    render(){
+        return <Template>
+            <div className={"naslovbox_desno"}>
+                <div className={"lom_naslovi naslov_desno"}> Nalazi privatnih ustanova </div>
+            </div>
+            <div className={"bigboy_left_smallboi_right"}>
+                <div className={"bigboy_left"}>
+                    <div className={"patient_list_instance"}>
+                        <div className={"patient_list_infobox"}>
+                            <div className={"patient_list_ime_prezime ped_text_color"}>Prezime Ime</div>
+                            <div className={"patient_list_oib"}>datum nalaza</div>
+                        </div>
+                        <div className={"patient_list_buttonbox"}>
+                            <button className={"patient_list_button ped_button_color"}>Otvori</button>
+                        </div>
+                    </div>
+                    <div className={"patient_list_instance"}>
+                        <div className={"patient_list_infobox"}>
+                            <div className={"patient_list_ime_prezime ped_text_color"}>Prezime Ime</div>
+                            <div className={"patient_list_oib"}>datum nalaza</div>
+                        </div>
+                        <div className={"patient_list_buttonbox"}>
+                            <button className={"patient_list_button ped_button_color"}>Otvori</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={"smallboi_right"}>
+                    <div className={"smallboi_infobox"}>
+                        <div id={"patient_name_textbox"}>Prezime Ime</div>
+                        <div id={"patient_oib_textbox"}>OIB</div>
+                    </div>
+                </div>
+            </div>
+        </Template>
+    }
+}
+
+export default ProfilePediatricianMedicalReport
---
+++ frontend/src/pages/ProfilePediatricianPatientProfile.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import Template from "../components/Template";
+import "../index.css";
+
+class ProfilePediatricianPatientProfile extends React.Component {
+    render(){
+        return <Template>
+            <div className="main">
+                <div className={"naslovbox_desno"}>
+                    <div className="lom_naslovi ped_patient_profile_title">Prezime Ime - Profil</div>
+                </div>
+
+                <div id="ped_main_container">
+                    <div>
+                        <div className={"lom_naslovi doctor_patient_profile_subtitle"}>Osnovni podaci</div>
+                        <div className={"flexbox"}>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Ime</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>Ime</div>
+                            </div>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Prezime</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>Prezime</div>
+                            </div>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>OIB</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>12345678910</div>
+                            </div>
+                        </div>
+                        <div className={"flexbox"}>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Datum rođenja</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>14/01/1990</div>
+                            </div>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Ime odgojno-obrazovne ustanove</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>Ime odgojno-obrazovne ustanove</div>
+                            </div>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Kontakt odgojno-obrazovne ustanove</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>Kontakt odgojno-obrazovne ustanove</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id={"doctor_patient_profile_line"} className="line"></div>
+                    <div>
+                        <div className={"lom_naslovi doctor_patient_profile_subtitle"}>Kontakt podaci roditelja</div>
+                        <div className={"flexbox"}>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Ime i prezime</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>Ime i prezime</div>
+                            </div>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Mjesto prebivališta</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>Mjesto prebivališta</div>
+                            </div>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Poštanski broj prebivališta</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>Poštanski broj prebivališta</div>
+                            </div>
+
+                        </div>
+                        <div className={"flexbox"}>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Broj telefona</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>099 1234 567</div>
+                            </div>
+                            <div className={"flexbox_column doctor_patient_profile"}>
+                                <div className={"doctor_patient_profile_tekst1"}>Adresa elektroničke pošte</div>
+                                <div className={"doctor_patient_profile_tekst2 ped_text_color"}>ime.prezime@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </Template>
+    }
+}
+
+export default ProfilePediatricianPatientProfile
