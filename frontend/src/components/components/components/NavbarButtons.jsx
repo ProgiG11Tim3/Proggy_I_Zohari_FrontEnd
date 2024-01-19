89488e5181c922a809f00933eff984dcbae28a60
Ante Prolić <ante.prolic2002@gmail.com>
2024-01-19 14:26:05 +0100

    Popravljena responzivnost za pacijenta i dodan collapsable menu u mobilnoj verziji

--- frontend/src/components/components/components/NavbarButtons.jsx
+++ frontend/src/components/components/components/NavbarButtons.jsx
@@ -4,34 +4,23 @@ import { Link } from "react-router-dom";
 import "../../../index.css";
 
 const NavbarButtons = (props) => {
-    const [collapsed, setCollapsed] = useState(false);
-    const [style, setStyle] = useState("collapse");
-    const [third, setThird] = useState("Potvrđena bolovanja");
-    const [buttons, setButtons] = useState(
-        <div id="nav_button_container">
-            <div className="nav_button" onClick={() => props.isSelected("0")}>Obavijesti</div>
-            <div className="nav_button" onClick={() => props.isSelected("1")}>Povijest liječničkih pregleda</div>
-            <div className="nav_button" onClick={() => props.isSelected("2")}>{third}</div>
-            <div className="nav_button" onClick={() => props.isSelected("3")}>Specijalistički pregledi</div>
-            <div className="nav_button" onClick={() => props.isSelected("4")}>Učitavanje nalaza</div>
-        </div>
-    )
-
-    const changeStyle = () => {
-        if(collapsed == true) {
-            console.log(style)
-            setStyle("visible");
-            setCollapsed(false);
-        } else {
-            console.log(style)
-            setStyle("collapse");
-            setCollapsed(true);
-        }
-    }
+    const [style, setStyle] = useState("visible");
+    const [buttons, setButtons] = useState(null);
 
     useEffect(() => {
+        if(window.innerWidth <= 768) {
+            setStyle("collapse");
+        }
         if(props.role == "Child") {
-            setThird("Generirane ispričnice ");
+            setButtons(
+                <div id="nav_button_container">
+                    <div className="nav_button" onClick={() => props.isSelected("0")}>Obavijesti</div>
+                    <div className="nav_button" onClick={() => props.isSelected("1")}>Povijest liječničkih pregleda</div>
+                    <div className="nav_button" onClick={() => props.isSelected("2")}>Generirane ispričnice</div>
+                    <div className="nav_button" onClick={() => props.isSelected("3")}>Specijalistički pregledi</div>
+                    <div className="nav_button" onClick={() => props.isSelected("4")}>Učitavanje nalaza</div>
+                </div>
+            )
         } else if (props.role == "Doktor") {
             setButtons(
                 <div id="nav_button_container">
@@ -70,12 +59,32 @@ const NavbarButtons = (props) => {
                     <Link to={"/pediatrician/patientprofile/"+props.oib+"/sendnotif"} className="nav_button">Obavijest roditelju</Link>
                 </div>
             )
+        } else if (props.role == "Parent") {
+            setButtons(
+                <div id="nav_button_container">
+                    <div className="nav_button" onClick={() => props.isSelected("0")}>Obavijesti</div>
+                    <div className="nav_button" onClick={() => props.isSelected("1")}>Povijest liječničkih pregleda</div>
+                    <div className="nav_button" onClick={() => props.isSelected("2")}>Potvrđena bolovanja</div>
+                    <div className="nav_button" onClick={() => props.isSelected("3")}>Specijalistički pregledi</div>
+                    <div className="nav_button" onClick={() => props.isSelected("4")}>Učitavanje nalaza</div>
+                </div>
+            )
         }
     }, [props])
 
+    const renderButtons = (buttons, style) => {
+        return <div className="collapsable" style={{visibility: style}}>{buttons}</div>;
+    }
+
     return <div id="nav_buttons">
-        <div id="mobile_menu" onClick={changeStyle}></div>
-        {buttons}
+        <div id="mobile_menu" onClick={() => {
+            if(style == "collapse") {
+                setStyle("visible");
+            } else {
+                setStyle("collapse");
+            }
+        }}></div>
+        {renderButtons(buttons, style)}
     </div>
 }
 
